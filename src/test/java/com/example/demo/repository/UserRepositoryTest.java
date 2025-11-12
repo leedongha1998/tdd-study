2bbf612ad316a22ef9c2870fbfd1b5316132f8c8
leedongha1998 <eedongha@naver.com>
2025-11-12 21:35:04 +0900

    userService에 대한 테스트 코드 추가

--- src/test/java/com/example/demo/repository/UserRepositoryTest.java
+++ src/test/java/com/example/demo/repository/UserRepositoryTest.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DataJpaTest(showSql=true)
+@DataJpaTest(showSql=true, properties = "classpath:test-application.properties")
 @Sql("/sql/user-repository-test-data.sql")
 public class UserRepositoryTest {
 
